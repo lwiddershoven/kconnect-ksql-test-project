40f48e14ac99ae875363269e0c2a5ed02791653d
Leon Widdershoven <github@qaz.dds.nl>
2021-03-02 16:33:56 +0100

    Added postgres, h2, springdoc, and micrometer dependencies. Enabled jpa repositories. mvn install works

--- customerservice/src/main/java/nl/leonw/kafkademo/customerservice/CustomerserviceApplication.java
+++ customerservice/src/main/java/nl/leonw/kafkademo/customerservice/CustomerserviceApplication.java
@@ -2,8 +2,10 @@ package nl.leonw.kafkademo.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class CustomerserviceApplication {
 
 	public static void main(String[] args) {
