af339838ed4851638ba16b60b5b6450bee226046
Leon Widdershoven <github@qaz.dds.nl>
2021-03-02 14:26:15 +0100

    Base services from start.spring.io

---
+++ customerservice/src/test/java/nl/leonw/kafkademo/customerservice/CustomerserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.leonw.kafkademo.customerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
