931d24116105777e23e1b0c2cc65af933c03c2c8
Leon Widdershoven <github@qaz.dds.nl>
2021-03-03 19:49:45 +0100

    shedlock: added explicit lockcheck to ensure that the locking annotation has actually been processed.

--- customerservice/src/main/java/nl/leonw/kafkademo/customerservice/sched/Scheduler.java
+++ customerservice/src/main/java/nl/leonw/kafkademo/customerservice/sched/Scheduler.java
@@ -1,6 +1,7 @@
 package nl.leonw.kafkademo.customerservice.sched;
 
 import lombok.extern.slf4j.Slf4j;
+import net.javacrumbs.shedlock.core.LockAssert;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,6 +13,7 @@ public class Scheduler {
     @Scheduled(fixedRate = 1000L)
     @SchedulerLock(name = "TaskScheduler_scheduledTask")
     void bla() {
+        LockAssert.assertLocked(); // Optional. Ensures the annotation is actually processed.
         log.info("Doing scheduled task");
     }
 
